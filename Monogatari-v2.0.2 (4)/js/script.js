f32b63ef80168226e81f5edae10b2899941c0391
Clamclim <clemedan@gmail.com>
2021-06-12 15:12:31 +0200

    crédits

--- Monogatari-v2.0.2 (4)/js/script.js
+++ Monogatari-v2.0.2 (4)/js/script.js
@@ -151,20 +151,29 @@ monogatari.action ('canvas').objects ({
 // Credits of the people involved in the creation of this awesome game
 monogatari.configuration ('credits', {
 "Développement":{
-	"Antonin Wyss":"Clémence Danesi", 
+	"Équipe":[
+		"Clémence Danesi", "Antonin Wyss",
+	],
 },
-"Bibliographie":{
-	"Beaud, S. & Weber, F. (2013). Guide de l’enquête de terrain : produire et analyser des données ethnographiques. Paris: La Découverte.":"",
-	"Becker, H. (2002). Les ficelles du métier. Comment conduire sa recherche en sciences sociales. Paris: La Découverte.":"",
-	"Bernard. S. (2011). Être caissière (caissier). Lyon: Éditions Lieux Dits, collection Être.":"",
-	"Blanchet, A. & al. (1985). L’entretien dans les sciences sociales. L’écoute, la parole et le sens.  Paris: Bordas.":"",
-	"Quivy, R. & Van Campenhoudt, L. (2017). Manuel de recherche en sciences sociales. Paris: Dunod.":"",
-	"Pinçon, M. & Pinçon-Charlot, M. (2005). Voyage en grande bourgeoisie: Journal d’enquête. Paris cedex 14, France: Presses Universitaires de France.":"",
-	"Laurens, S. (2007). 'Pourquoi et comment poser le questions qui fâchent ?' Réflexion sur les dilemmes récurrents que posent les entretiens avec les imposants. Genèses, n° 69, p. 112- 127.":"",
-	"Mayer, N. (1995). L'entretien selon Pierre Bourdieu. Analyse critique de La misère du monde. Revue française de sociologie, 36-2. pp. 355-370.":"",
-	"Sauvayre, R. (2013). Chapitre 2. La réalisation de l’entretien : le « terrain ». Dans : , R. Sauvayre, Les méthodes de l'entretien en sciences sociales (pp. 49-101). Paris: Dunod.":"",
-	"Waelli, M. (2009). Caissière... et après : Une enquête parmi les travailleurs de la grande distribution. Paris cedex 14, France: Presses Universitaires de France.":"",
+"Références":{
+	"Bibliographie":[
+	"Beaud, S. & Weber, F. (2013). Guide de l’enquête de terrain : produire et analyser des données ethnographiques. Paris: La Découverte.",
+	"Becker, H. (2002). Les ficelles du métier. Comment conduire sa recherche en sciences sociales. Paris: La Découverte.",
+	"Bernard. S. (2011). Être caissière (caissier). Lyon: Éditions Lieux Dits, collection Être.",
+	"Blanchet, A. & al. (1985). L’entretien dans les sciences sociales. L’écoute, la parole et le sens.  Paris: Bordas.",
+	"Quivy, R. & Van Campenhoudt, L. (2017). Manuel de recherche en sciences sociales. Paris: Dunod.",
+	"Pinçon, M. & Pinçon-Charlot, M. (2005). Voyage en grande bourgeoisie: Journal d’enquête. Paris cedex 14, France: Presses Universitaires de France.",
+	"Laurens, S. (2007). 'Pourquoi et comment poser le questions qui fâchent ?' Réflexion sur les dilemmes récurrents que posent les entretiens avec les imposants. Genèses, n° 69, p. 112- 127.",
+	"Mayer, N. (1995). L'entretien selon Pierre Bourdieu. Analyse critique de La misère du monde. Revue française de sociologie, 36-2. pp. 355-370.",
+	"Sauvayre, R. (2013). Chapitre 2. La réalisation de l’entretien : le « terrain ». Dans : , R. Sauvayre, Les méthodes de l'entretien en sciences sociales (pp. 49-101). Paris: Dunod.",
+	"Waelli, M. (2009). Caissière... et après : Une enquête parmi les travailleurs de la grande distribution. Paris cedex 14, France: Presses Universitaires de France.",
+	],
 },
+
+"Contributeur-trice-s":{
+	"Images":" puppetbomb ",
+	"Musique et sons":" Fluminislctus ",
+}
 
 });
 
