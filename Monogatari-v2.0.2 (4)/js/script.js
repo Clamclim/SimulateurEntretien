2e9c474c0c9b9b6776f368d1dd2d1ba6be7c74db
Clamclim <clemedan@gmail.com>
2021-06-13 15:14:14 +0200

    Update script.js

--- Monogatari-v2.0.2 (4)/js/script.js
+++ Monogatari-v2.0.2 (4)/js/script.js
@@ -161,21 +161,22 @@ monogatari.configuration ('credits', {
 	"Équipe":[
 		"Clémence Danesi", "Antonin Wyss",
 	],
+
+	"Github":[
+		"<a href=https://github.com/Clamclim/Visual-Novel.git> Références </a>",
+	]
 },
 
 "Contributeur-trice-s":{
-	"Images caractère":"<a href=https://puppetbomb.itch.io/>puppetbomb",
-	"Images décors":"<a href=https://cloudnovel.net/fluffness>fluffness",
-	"Musiques et sons":"<a href=https://fulminisictus.itch.io/>Fluminislctus",
-}
+	"Images caractère":"<a href=https://puppetbomb.itch.io/> puppetbomb </a>",
+	"Images décors":"<a href=https://cloudnovel.net/fluffness> fluffness </a>",
+	"Musiques et sons":"<a href=https://fulminisictus.itch.io/> Fluminislctus </a>"
+},
+
+
 
 });
 
-
-// Define the images that will be available on your game's image gallery
-monogatari.assets ('gallery', {
-
-});
 
 // Define the music used in the game.
 monogatari.assets ('music', {
@@ -186,10 +187,6 @@ monogatari.assets ('music', {
 
 });
 
-// Define the voice files used in the game.
-monogatari.assets ('voices', {
-
-});
 
 // Define the sounds used in the game.
 monogatari.assets ('sounds', {
@@ -197,15 +194,7 @@ monogatari.assets ('sounds', {
 
 });
 
-// Define the videos used in the game.
-monogatari.assets ('videos', {
 
-});
-
-// Define the images used in the game.
-monogatari.assets ('images', {
-
-});
 
 // Define the backgrounds for each scene.
 monogatari.assets ('scenes', {
